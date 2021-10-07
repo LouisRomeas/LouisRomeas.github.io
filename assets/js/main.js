b3aaa3ba331c6188beda32b450b8654b99fb420b
LouisRomeas <louis.romeas@gmail.com>
2021-10-08 00:13:33 +0200

    Update main.js
    forgot brackets

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ window.onscroll = function() {scrollFunction(25)};
 particlesJS.load('particles-js', '/assets/js/particles.json');
 
 fetch('https://apimaker.alwaysdata.net/api/?host=mysql-apimaker.alwaysdata.net&db=apimaker_mess&table=menus_cantine&user=apimaker_reader&pwd=D4t4p1!')
-    .then(res => res.json)
+    .then(res => res.json())
     .then((result) => {
         console.log(result)
     });
