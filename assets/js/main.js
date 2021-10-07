0b91f2699a2cf24556ede562591a45b4837151b6
LouisRomeas <louis.romeas@gmail.com>
2021-10-08 00:10:42 +0200

    Update main.js
    added fetch test

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ import scrollFunction from './modules/scroll.js';
 window.onscroll = function() {scrollFunction(25)};
 
 particlesJS.load('particles-js', '/assets/js/particles.json');
+
+fetch('https://apimaker.alwaysdata.net/api/?host=mysql-apimaker.alwaysdata.net&db=apimaker_mess&table=menus_cantine&user=apimaker_reader&pwd=D4t4p1!')
+    .then(res => res.json)
+    .then((result) => {
+        console.log(result)
+    });
